2ebe97a0ab9f27e31b32874633ef863a1f889a88
Mr Martian <craiggles@s2maps.io>
2025-08-05 17:45:05 -0400

    100% test coverage for existing gbfs

--- tests/readers/gbfs/mod.rs
+++ tests/readers/gbfs/mod.rs
@@ -4,7 +4,14 @@ mod v1_1;
 // #[coverage(off)]
 #[cfg_attr(feature = "nightly", coverage(off))]
 mod tests {
-    use gistools::readers::{GBFSSystem, parse_gtfs_systems};
+    use gistools::readers::{GBFSSystem, gbfs_bool_or_int, parse_gtfs_systems};
+    use serde::de::Error as _;
+    use serde::{
+        Deserialize, Deserializer,
+        de::{Unexpected, Visitor},
+        forward_to_deserialize_any,
+    };
+    use serde_json::{from_value, json};
     use std::{fs, path::PathBuf};
 
     #[test]
@@ -74,5 +81,96 @@ mod tests {
                 }
             ]
         );
+    }
+
+    #[test]
+    fn test_gbfs_bool_or_int() {
+        #[derive(Debug, Deserialize)]
+        struct Wrapper {
+            #[serde(deserialize_with = "gbfs_bool_or_int")]
+            value: bool,
+        }
+
+        // true
+        let val: Wrapper = from_value(json!({ "value": true })).unwrap();
+        assert!(val.value);
+
+        // false
+        let val: Wrapper = from_value(json!({ "value": false })).unwrap();
+        assert!(!val.value);
+
+        // 1 u64
+        let val: Wrapper = from_value(json!({ "value": 1_u64 })).unwrap();
+        assert!(val.value);
+
+        // 0 u64
+        let val: Wrapper = from_value(json!({ "value": 0_u64 })).unwrap();
+        assert!(!val.value);
+
+        // 1 i64
+        let val: Wrapper = from_value(json!({ "value": 1_i64 })).unwrap();
+        assert!(val.value);
+
+        // -1 i64
+        let val: Result<Wrapper, _> = from_value(json!({ "value": -1_i64 }));
+        assert!(val.is_err());
+
+        // 0 i64
+        let val: Wrapper = from_value(json!({ "value": -0_i64 })).unwrap();
+        assert!(!val.value);
+
+        // string true
+        let val: Wrapper = from_value(json!({ "value": "true" })).unwrap();
+        assert!(val.value);
+
+        // string false
+        let val: Wrapper = from_value(json!({ "value": "false" })).unwrap();
+        assert!(!val.value);
+
+        // string 0
+        let val: Wrapper = from_value(json!({ "value": "0" })).unwrap();
+        assert!(!val.value);
+
+        // string 1
+        let val: Wrapper = from_value(json!({ "value": "1" })).unwrap();
+        assert!(val.value);
+
+        // error
+        let res: Result<Wrapper, _> = from_value(json!({ "value": 3 }));
+        assert!(res.is_err());
+
+        let res: Result<Wrapper, _> = from_value(json!({ "value": "yes" }));
+        assert!(res.is_err());
+    }
+
+    #[test]
+    fn test_expectation_error_triggers() {
+        struct Dummy;
+
+        impl<'de> Deserializer<'de> for Dummy {
+            type Error = serde::de::value::Error;
+
+            fn deserialize_any<V>(self, visitor: V) -> Result<V::Value, Self::Error>
+            where
+                V: Visitor<'de>,
+            {
+                // Trigger error using invalid type
+                Err(Self::Error::invalid_type(Unexpected::Unit, &visitor))
+            }
+
+            // implement all other deserialize_* as unimplemented so `deserialize_any` is called
+            forward_to_deserialize_any! {
+                bool i8 i16 i32 i64 u8 u16 u32 u64 f32 f64 char str string bytes byte_buf
+                option unit unit_struct newtype_struct seq tuple
+                tuple_struct map struct enum identifier ignored_any
+            }
+        }
+
+        let result = gbfs_bool_or_int(Dummy);
+        assert!(result.is_err());
+
+        // Optional: verify that the error contains the string from `expecting`
+        let err_msg = result.unwrap_err().to_string();
+        assert!(err_msg.contains("a boolean or an integer 0/1"));
     }
 }
